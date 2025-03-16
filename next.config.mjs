b33ca205d3c83f9cb3380e3c13a4ea1c4c750264
codingnoobno1 <164862821+codingnoobno1@users.noreply.github.com>
2025-03-16 17:25:04 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "drive.google.com",
+        },
+        {
+          protocol: "https",
+          hostname: "lh3.googleusercontent.com", // Sometimes Google Drive serves images from this domain
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
